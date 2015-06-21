b98cea6e6195a35a94aa440dc44f976d67f2f151
Joona <joona.paivahonka@gmail.com>
2015-06-21 17:47:03 +0300

    fixed some brain farts and probably added few new ones

--- src/irc_cmds.c
+++ src/irc_cmds.c
@@ -7,11 +7,9 @@
 
 int privmsg(char *msg, char *target, server *srv)
 {
-	//PRIVMSG target :msg
 	static pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 
 	pthread_mutex_lock(&mutex);
-	usleep(75);
 	char *buf = malloc(( 7 + 1 + strlen(target) + 2 + strlen(msg) + 2 + 1) * sizeof(char));
 	sprintf(buf,"PRIVMSG %s :%s\r\n", target, msg);
 	int res = server_send(buf, srv);
@@ -27,19 +25,27 @@ int join_channel(char *chan_name, char *chan_pass, server *srv)
 	static pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 
 	pthread_mutex_lock(&mutex);
-	usleep(75);
-    char buf[1024];
+    char *buf = NULL;
     if (chan_pass == NULL)
     {
+		buf = malloc((4 + 1 + strlen(chan_name) + 2 + 1) * sizeof(char));
         sprintf(buf, "JOIN %s\r\n", chan_name);
     }
     else
     {
+		buf = malloc((4 + 1 + strlen(chan_name) + 2 + strlen(chan_pass) + 2 + 1) * sizeof(char));
         sprintf(buf, "JOIN %s; %s\r\n", chan_name, chan_pass);
     }
 
+	sprintf(buf, buf, chan_name);
+
+	int len = strlen(buf);
+	char buf_copy[len];
+	strcpy(buf_copy, buf);
+
+
 	pthread_mutex_unlock(&mutex);
-    return server_send(buf, srv);
+    return server_send(buf_copy, srv);
 }
 
 
@@ -48,12 +54,16 @@ int part_channel(char *reason, channel *chan)
 	static pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 
 	pthread_mutex_lock(&mutex);
-	usleep(75);
     char *buf = "PART %s\r\n";
     buf = malloc((strlen(buf) + strlen(chan->name)) * sizeof(char));
     sprintf(buf, buf, chan->name);
+
+	int len = strlen(buf);
+	char buf_copy[len];
+	strcpy(buf_copy, buf);
 	free(buf);
+
 	pthread_mutex_unlock(&mutex);
 
-    return server_send(buf, chan->srv);
+    return server_send(buf_copy, chan->srv);
 }
