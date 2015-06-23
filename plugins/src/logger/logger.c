ae94b569d3c8b2014e28c10281857e5a953e68f0
Joona <joona.paivahonka@gmail.com>
2015-06-23 16:19:29 +0300

    added test function for debugging purposes

--- plugins/src/logger/logger.c
+++ plugins/src/logger/logger.c
@@ -10,12 +10,14 @@ char plugin_author[] = "Joona";
 
 int on_server_send(void **paramams, int argc);
 int on_connect(void **params, int argc);
+int test(void **params, int argc);
 
 
 int plugin_init(plugin *pluginptr)
 {
     register_callback( CALLBACK_SERVER_CONNECTED, on_connect, pluginptr);
     register_callback( CALLBACK_SERVER_RECV, on_server_send, pluginptr );
+    register_callback( CALLBACK_GOT_PRIVMSG, test, pluginptr );
 
     return BCIRC_PLUGIN_OK;
 }
@@ -38,6 +40,20 @@ int on_server_send(void **paramams, int argc)
         return BCIRC_PLUGIN_BREAK;
 
     printf("%s\n", buf);
+
+    return BCIRC_PLUGIN_OK;
+}
+
+int test(void **params, int argc)
+{
+    server *srv = params[0];
+    char *channel = params[3];
+    char *msg = params[4];
+
+    if (strcmp(msg, "#tesm") != 0)
+        return BCIRC_PLUGIN_OK;
+        
+    add_to_privmsg_queue(msg, target, srv, 0);
 
     return BCIRC_PLUGIN_OK;
 }
