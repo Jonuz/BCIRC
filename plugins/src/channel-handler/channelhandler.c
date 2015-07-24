0138f431db9113960ec81784513189570f7cb33f
Joona <joona.paivahonka@gmail.com>
2015-07-24 16:14:24 +0300

    added plugin which handles channels, not ready yet thought

--- plugins/src/channel-handler/channelhandler.c
+++ plugins/src/channel-handler/channelhandler.c
@@ -1,40 +1,192 @@
 #include <string.h>
 #include <unistd.h>
 #include <stdio.h>
+#include <string.h>
 
+#include "../headers/numeric.h"
 #include "../headers/server.h"
 #include "../headers/plugin_handler.h"
 #include "../headers/callback_defines.h"
 
 char plugin_name[] = "BCIRC-Core - Channel handler";
 char plugin_author[] = "Joona";
-char plugin_version[] = "1.0";
+char plugin_version[] = "0.1";
 
-int register_channel(void **params, int argc);
+int get_channel_info(void **params, int argcv);
+
+int joinded_channel(channel *chan, char *buffer);
+int get_channel_title(channel *chan, char *buffer);
+int get_channel_title_info(channel *chan, char *buffer);
+int get_channel_users(channel *chan, char *buffer);
+
 
 int plugin_init(plugin *pluginptr)
 {
-    register_callback(CALLBACK_SERVER_RECV, register_channel, pluginptr);
+    register_callback(CALLBACK_GOT_NUMERIC, get_channel_info, 10, pluginptr);
+
+    return BCIRC_PLUGIN_OK;
+}
+
+int get_channel_info(void **params, int argcv)
+{
+    int *numeric = (int*) params[0];
+    char buffer[strlen(params[1])];
+    server *srv = params[2];
+
+    strcpy(buffer, params[1]);
+
+    if (!buffer)
+    {
+        puts("buffers is null!");
+        return BCIRC_PLUGIN_OK;
+    }
+
+    while(1)
+    {
+        if (*numeric == RPL_TOPIC)
+            break;
+        if (*numeric == RPL_ENDOFNAMES)
+            break;
+        if (*numeric == RPL_TOPICWHOTIME)
+            break;
+        if (*numeric == RPL_NAMREPLY)
+            break;
+        return BCIRC_PLUGIN_OK;
+    }
+
+
+    char *name_start = memchr(buffer, '#', strlen(buffer)); //Todo: use CHANTYPES
+
+    int len = 0;
+    for (len; name_start[len] != ' '; len++); {};
+
+    char *chan_name = malloc((len + 1) * sizeof(char));
+    memmove(chan_name, name_start, len);
+    chan_name[len] = '\0';
+
+    channel *this_channel = NULL;
+    if ((this_channel = get_channel(chan_name, srv)) == NULL)
+    {
+        this_channel = create_channel(chan_name, srv);
+        if (!this_channel)
+        {
+            printf("Failed to add channel named %s\n!", chan_name);
+            return BCIRC_PLUGIN_OK;
+        }
+    }
+
+
+    switch(*numeric)
+    {
+        case RPL_ENDOFNAMES:
+            return joinded_channel(this_channel, buffer);
+
+        case RPL_TOPIC:
+            return get_channel_title(this_channel, buffer);
+
+        case RPL_TOPICWHOTIME:
+            return get_channel_title_info(this_channel, buffer);
+
+        case RPL_NAMREPLY:
+            return get_channel_users(this_channel, buffer);
+
+        default:
+            break;
+    }
+
+    return BCIRC_PLUGIN_OK;
+}
+
+int joinded_channel(channel *chan, char *buffer)
+{
+
+    printf("\nJoined to channel %s\n", chan->name);
+    printf("Channel topic: %s\n", chan->topic);
+    printf("Topic created %d by %s\n", chan->topic_created_time, chan->topic_creator);
+    printf("Channel users: %s\n\n", chan->users);
 
     return BCIRC_PLUGIN_OK;
 }
 
 
-
-int register_channel(void **params, int argc)
+int get_channel_title(channel *chan, char *buffer)
 {
-    server *srv = params[0];
-    char buffer[strlen(params[1])];
-    int *numeric = params[2];
-
-    strcpy(buffer, params[1]);
-
-    if ((!srv) || (!buffer) || (!numeric))
-        return BCIRC_PLUGIN_BREAK;
-
-    if (*numeric != RPL_TOPIC)
+    if (!buffer)
+    {
+        puts("buffers is null!");
         return BCIRC_PLUGIN_OK;
+    }
 
 
+    char *title_start = memchr(buffer + 1, ':', strlen(buffer));
 
+    size_t title_len = strlen(buffer) - strlen(title_start);
+
+    chan->topic = malloc((title_len + 1) * sizeof(char));
+
+    memmove(chan->topic, title_start+1, title_len);
+    printf("%s\n", chan->topic);
+
+    return BCIRC_PLUGIN_OK;
+}
+
+
+/*
+    :irc.localhost 333 tosibotti2 #tesm joona!~joona@127.0.0.1 1437735223
+*/
+int get_channel_title_info(channel *chan, char *buffer)
+{
+    if (!buffer)
+    {
+        puts("buffers is null!");
+        return BCIRC_PLUGIN_OK;
+    }
+
+    char *token, *save;
+
+    char *buf_for_tokes = malloc((strlen(buffer) + 1) * sizeof(char));
+    strcpy(buf_for_tokes, buffer);
+
+    char *info_start = memchr(buf_for_tokes, '#', strlen(buffer));
+
+    token = strtok_r(info_start, " ", &save);
+
+    for (int i = 0; token != NULL; i++)
+    {
+        if (i == 1)
+        {
+            chan-> topic_creator = malloc((strlen(token) + 1) * sizeof(char));
+            strcpy(chan->topic_creator, token);
+        }
+        if (i == 2)
+        {
+            chan->topic_created_time = atoi(token);
+            return BCIRC_PLUGIN_OK;
+        }
+
+        token = strtok_r(NULL, " ", &save);
+    }
+
+    return BCIRC_PLUGIN_OK;
+}
+
+
+int get_channel_users(channel *chan, char *buffer)
+{
+    if (!buffer)
+    {
+        puts("buffers is null!");
+        return BCIRC_PLUGIN_OK;
+    }
+
+    char *users = memchr(buffer + 1, ':', strlen(buffer));
+    *users++;
+
+    size_t users_len = strlen(users);
+
+    chan->users = realloc(chan->users, (users_len + 1) * sizeof(char));
+    strcpy(chan->users, users);
+
+
+    return BCIRC_PLUGIN_OK;
 }
