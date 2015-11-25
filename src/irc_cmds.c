396ddc40b6fddbc0c0277bf8e9ced1fd46e30233
Joona <joona.paivahonka@gmail.com>
2015-11-25 23:18:31 +0200

    made something to to few irc cmd, this should have been commited much earlier

--- src/irc_cmds.c
+++ src/irc_cmds.c
@@ -7,39 +7,39 @@
 #include "../headers/channel.h"
 #include "../headers/irc_cmds.h"
 
-int privmsg(char *msg, char *target, server *srv)
-{
-	char *buf = malloc(( 7 + 1 + strlen(target) + 2 + strlen(msg) + 2 + 1) * sizeof(char));
-	sprintf(buf,"PRIVMSG %s :%s\r\n", target, msg);
-	int res = server_send(buf, srv);
-	free(buf);
-
-	return res;
-}
-
-int join_channel(char *chan_name, char *chan_pass, server *srv)
-{
-	char *buf = NULL;
-	if (chan_pass == NULL)
+	int privmsg(char *msg, char *target, server *srv)
 	{
-		buf = malloc((4 + 1 + strlen(chan_name) + 2 + 1) * sizeof(char));
-		sprintf(buf, "JOIN %s\r\n", chan_name);
-	}
-	else
-	{
-		buf = malloc((4 + 1 + strlen(chan_name) + 2 + strlen(chan_pass) + 2 + 1) * sizeof(char));
-		sprintf(buf, "JOIN %s; %s\r\n", chan_name, chan_pass);
+		char *buf = malloc(( 7 + 1 + strlen(target) + 2 + strlen(msg) + 2 + 1) * sizeof(char));
+		sprintf(buf,"PRIVMSG %s :%s\r\n", target, msg);
+		int res = server_send(buf, srv);
+		free(buf);
+
+		return res;
 	}
 
-	sprintf(buf, buf, chan_name);
+	int join_channel(char *chan_name, char *chan_pass, server *srv)
+	{
+		char *buf = NULL;
+		if (chan_pass == NULL)
+		{
+			buf = malloc((4 + 1 + strlen(chan_name) + 2 + 1) * sizeof(char));
+			sprintf(buf, "JOIN %s\r\n", chan_name);
+		}
+		else
+		{
+			buf = malloc((4 + 1 + strlen(chan_name) + 2 + strlen(chan_pass) + 2 + 1) * sizeof(char));
+			sprintf(buf, "JOIN %s; %s\r\n", chan_name, chan_pass);
+		}
 
-	int len = strlen(buf);
-	char buf_copy[len];
-	strcpy(buf_copy, buf);
-	free(buf);
+		sprintf(buf, buf, chan_name);
 
-	return server_send(buf_copy, srv);
-}
+		int len = strlen(buf);
+		char buf_copy[len];
+		strcpy(buf_copy, buf);
+		free(buf);
+	
+		return server_send(buf_copy, srv);
+	}
 
 
 int part_channel(char *reason, channel *chan)
