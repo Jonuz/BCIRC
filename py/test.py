e55f52c4f443cb740e91f04445de063ba7d82ae7
Joona <joona.paivahonka@gmail.com>
2015-12-08 20:59:27 +0200

    added test script

---
+++ py/test.py
@@ -0,0 +1,9 @@
+script_name = "test script"
+script_version = "0.1"
+script_authotr = "Joona"
+
+import bcirc
+
+def py_init(script):
+    #bcirc.register_script(script, script_name, script_version, script_author)
+    print("yo")
