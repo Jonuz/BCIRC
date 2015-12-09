8752fc2951de91800b17138724aad0fed0c24f29
Joona <joona@jhonka.me>
2015-12-09 13:47:40 +0100

    changed name of function

--- py/test.py
+++ py/test.py
@@ -1,9 +1,10 @@
 script_name = "test script"
 script_version = "0.1"
-script_authotr = "Joona"
+script_author = "Joona"
 
 import bcirc
 
-def py_init(script):
-    #bcirc.register_script(script, script_name, script_version, script_author)
+def script_init(script):
+    bcirc.register_script(script, script_name, script_version, script_author)
     print("yo")
+
