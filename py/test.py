39356a3e5a8b2383e66b4ae72c5fe4f108ea492b
Joona <joona.paivahonka@gmail.com>
2015-12-10 18:29:21 +0200

    added __init__.py to repo

--- py/test.py
+++ py/test.py
@@ -4,7 +4,10 @@ script_author = "Joona"
 
 import bcirc
 
+def on_recv(params, count):
+    print("server sent something")
+
 def script_init(script):
     bcirc.register_script(script, script_name, script_version, script_author)
+    bcirc.register_callback(script, "callback_server_recv", on_recv, 20)
     print("yo")
-
