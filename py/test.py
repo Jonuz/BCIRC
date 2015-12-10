b428fe180fda421b32b081f526069ae6769dccac
Joona <joona.paivahonka@gmail.com>
2015-12-10 23:13:15 +0200

    added return value

--- py/test.py
+++ py/test.py
@@ -10,4 +10,5 @@ def on_recv(params, count):
 def script_init(script):
     bcirc.register_script(script, script_name, script_version, script_author)
     bcirc.register_callback(script, "callback_server_recv", on_recv, 20)
-    print("yo")
+
+    return 1;
