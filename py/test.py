e2c4eb25fa70f7f162e5cda874848e8c4f29abb4
Joona <joona.paivahonka@gmail.com>
2015-12-11 03:19:55 +0200

    see

--- py/test.py
+++ py/test.py
@@ -3,12 +3,16 @@ script_version = "0.1"
 script_author = "Joona"
 
 import bcirc
+from ctypes import *
 
 def on_recv(params, count):
-    print("server sent something")
+    #buf = c_char_p(params[1])
+    #rint("buf: ", buf)
+    print ("something sent")
+
 
 def script_init(script):
     bcirc.register_script(script, script_name, script_version, script_author)
-    bcirc.register_callback(script, "callback_server_recv", on_recv, 20)
+    bcirc.register_callback(script, "server_recv", on_recv, 20)
 
-    return 1;
+    return 1
