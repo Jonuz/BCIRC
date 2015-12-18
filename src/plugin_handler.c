6bc5d3af9e1e04154283df4252642c2f9f9af596
Joona <joona.paivahonka@gmail.com>
2015-12-18 17:28:33 +0200

    removed undeeded and wrong thing from remove_index()

--- src/plugin_handler.c
+++ src/plugin_handler.c
@@ -137,7 +137,7 @@ int get_plugins(char *plugin_dir)
                 if (strlen(dir->d_name) > 3)
                 {
                     size_t name_len = strlen(dir->d_name);
-                    if (strstr(dir->d_name, ".so") == dir->d_name+name_len-3) //name ends to .so
+                    if (strstr(dir->d_name, ".so") == dir->d_name+name_len-3) //name of file ends to .so
                     {
                         char *plugin_to_add = malloc( (strlen(plugin_dir) + 1 + strlen(dir->d_name) + 1) * sizeof(char));
                         sprintf(plugin_to_add, "%s/%s", plugin_dir, dir->d_name);
@@ -354,7 +354,6 @@ int remove_index(callback *cb_ptr)
             else
             {
                 free(index_list[i]->callbacks[y]);
-                index_list[i]->callbacks[y] = NULL;
             }
         }
         index_list[i]->cb_count = new_count;
